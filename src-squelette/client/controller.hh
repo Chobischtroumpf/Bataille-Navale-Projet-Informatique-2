685435d8e1947e01588169d670196a2eb5eb48e8
Romain Croughs <romain.croughs@ulb.be>
2024-02-17 17:20:02 +0100

    fix: Refactor Controller class to remove unused dependencies

--- src-squelette/client/controller.hh
+++ src-squelette/client/controller.hh
@@ -1,17 +1,11 @@
 #pragma once
 
-#include "../common/board_coordinates.hh"
-#include "../common/ship_coordinates.hh"
-#include "display.hh"
-#include <memory>
+#include <string>
 
 /*
  * Interface du Controller (ce qui permet d'envoyer des données)
  */
 class Controller {
 public:
-  virtual void setDisplay(std::shared_ptr<Display>) = 0;
-  virtual void connectServer() = 0;
-  virtual bool fire(BoardCoordinates) const = 0;
-  virtual bool placeShip(ShipCoordinates) const = 0;
+  virtual void sendData(std::string data) {}
 };
