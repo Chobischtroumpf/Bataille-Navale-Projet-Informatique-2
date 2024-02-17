a6086c77ac62b627af6b71bcbbb5fc713fb8008a
pjadczuk <pablojadczuk1999@wp.pl>
2024-02-17 22:20:38 +0100

    ajout du login_controller pour matcher sur la compilation, (juste des return true)

---
+++ src-squelette/client/Controllers/login_controller.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "login_controller.hh"
+
+LoginController::bool attemptLogin(std::string username, std::string password) const{
+	return true;
+}
+
+bool checkValidity(std::string username, std::string password) const{
+	return true;
+}
