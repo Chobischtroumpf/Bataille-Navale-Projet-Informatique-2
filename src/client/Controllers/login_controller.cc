cf1e1c86545dfba4f2c5af556b5a6a66bb26ef04
pjadczuk <pablojadczuk1999@wp.pl>
2024-03-02 20:46:37 +0100

    maj, compilation

--- src/client/Controllers/login_controller.cc
+++ src/client/Controllers/login_controller.cc
@@ -2,10 +2,10 @@
 
 #include "../../../include/client/Controllers/login_controller.hh"
 
-LoginController::bool attemptLogin(std::string username, std::string password) const{
+bool LoginController::attemptLogin(std::string username, std::string password) const{
 	return true;
 }
 
-bool checkValidity(std::string username, std::string password) const{
+bool LoginController::checkValidity(std::string username, std::string password) const{
 	return true;
 }
