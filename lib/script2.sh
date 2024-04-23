c50e950ca6311d44dcb9d87bb7ded71a69fb12f8
Alessandro Dorigo <alessandro.dorigo@ulb.be>
2024-04-23 16:33:39 +0200

    modif script2, devrait chopper tout les pckg

--- lib/script2.sh
+++ lib/script2.sh
@@ -1,16 +1,14 @@
+#!/bin/bash
 
+packages=("libcpprest-dev" "qt6-base-dev" "libsodium-dev")
 
+for package in $packages; do 
+    rm -rf "output.txt"
+    touch "output.txt"
+    apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances $package | grep "^\w" | sort -u >> output.txt
+done
 
-if [ $# -gt 0 ]; then
-    package=$1
+output=$(xargs apt list < output.txt | grep -v "installed" | grep -v "upgradable" | awk '{print $1}' | awk -F'/' '{print $1}' | sed 's/Listing...//')
 
-    apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances $package | grep "^\w" | sort -u > output.txt
-
-    output=$(xargs apt list < output.txt | grep -v "installed" | grep -v "upgradable" | awk '{print $1}' | awk -F'/' '{print $1}' | sed 's/Listing...//')
-    
-    echo $output > output.txt
-    xargs apt-get download  < output.txt
-
-else
-    echo "No argument provided."
-fi
+echo $output > output.txt
+xargs apt-get download  < output.txt
